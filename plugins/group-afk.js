d6d8a941bdc4ec5a3587b09c362b8f20bbad0c35
VenBotz <99535100+VenBotz@users.noreply.github.com>
2022-07-28 07:55:09 +0800

    Add files via upload

--- plugins/group-afk.js
+++ plugins/group-afk.js
@@ -1,22 +1,14 @@
-let handler = (m, { text }) => {
-  let user = global.DATABASE.data.users[m.sender]
+let handler = async (m, { text }) => {
+  let user = global.db.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
   m.reply(`
-╭══════════〔  *AFK MODE*  〕══════════
-║${conn.getName(m.sender)} Sekarang sedang AFK!
-║
-║Alasan : ${text ? '' + text : ''}
-║
-║
-║                                
-╰═══════════〔  *VENBOTZ*  〕══════════
+${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
 `)
-conn.sendButton(m.chat, str, wm, 'Jangan Ganggu Ya !', 'Iyaa',m)
-conn.reply(str)
 }
-handler.help = ['afk <alasan>']
+handler.help = ['afk [alasan]']
 handler.tags = ['group']
 handler.command = /^afk$/i
+handler.limit = false
 
 module.exports = handler
